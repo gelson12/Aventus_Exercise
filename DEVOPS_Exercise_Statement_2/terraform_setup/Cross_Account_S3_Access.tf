b9fd318840bcd1de11b5ae6f743a58aeeeeee8e6
gelson <gelson_m@hotmail.com>
2024-03-23 10:55:15 +0000

    correcting the error from Cross_Account_S3_Access.tf which is to replace the incorrect aws_s3_bucket_acl resource definition with an aws_s3_bucket_policy resource definition that properly applies the policy to your S3 bucket.

--- DEVOPS_Exercise_Statement_2/terraform_setup/Cross_Account_S3_Access.tf
+++ DEVOPS_Exercise_Statement_2/terraform_setup/Cross_Account_S3_Access.tf
@@ -1,13 +1,12 @@
-resource "aws_s3_bucket_acl" "b" {
+resource "aws_s3_bucket_policy" "b_policy" {
   bucket = "aventus-bucket-in-account-b"
-  acl    = "private"
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
       Principal = {
-        AWS: "arn:aws:iam::123456789012:role/lambda_execution_role",//Replace 123456789012 with your actual AWS Account ID where the Lambda function resides.
+        AWS = "arn:aws:iam::123456789012:role/lambda_execution_role", # Replace 123456789012 with your actual AWS Account ID where the Lambda function resides.
       },
       Action = "s3:PutObject",
       Resource = "arn:aws:s3:::aventus-bucket-in-account-b/*",
